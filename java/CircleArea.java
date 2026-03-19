08ee2f37d3c4c49f4da2ef59c7ee683d815bc82b
EunYoung520 <165741784+EunYoung520@users.noreply.github.com>
2026-03-19 14:16:36 +0900

    Refactor CircleArea variable names and comments
    Updated variable naming conventions and added comments for clarity.

--- java/CircleArea.java
+++ java/CircleArea.java
@@ -1,6 +1,7 @@
 public class CircleArea {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { //현재 코드에선 string[] args가 필요 없으나 java 자체에서 string[] args가 들어가야만 main의 자리로 인정
+		//main을 사용할 때 무조건 public static void main의 구성을 갖춰야만 java에서 인정해준다. 
 		// TODO Auto-generated method stub
 		//원의 넓이와 둘레의 길이
 		final double PI = 3.14; //final이 앞에 들어가면 상수가 된다
@@ -9,9 +10,12 @@ public class CircleArea {
 		double r = 10.0; // 원의 반지름
     //변수로 선언했기에 아래에 r=1.0따위의 줄을 추가해도 변경이 가능하다 
 		
-		double CircleArea = r*r*PI; 
+		double circleArea = r*r*PI; //변수 이름은 소문자로 시작하는 게 좋음
 		
-		System.out.println("원의 면적 =" + CircleArea); //print에 있어 타입 지정이 필요 없다. 
+		System.out.println("원의 면적 =" + circleArea); //print에 있어 타입 지정이 필요 없다. 
+		//System: 자바가 기본으로 제공하는 클래스, 컴퓨터 시스템 관련 기능 모아둠
+		//out: 출력
+		//System.out: 'System'안의 출력 창
 
 	}
 
