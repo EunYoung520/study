ea275b49e5a14f40106e2056f3b1cf274268f536
EunYoung520 <165741784+EunYoung520@users.noreply.github.com>
2026-04-01 14:29:45 +0900

    Refactor add function and main for clarity

--- cpp/template_add.cpp
+++ cpp/template_add.cpp
@@ -1,16 +1,13 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 template <typename T>
-T add(T a, T b) { return a + b; }
-
-int main() {
-	int x(2), y(3);
-	double i(2.5), j(3.7);
-
-	cout << add<int>(x, y) << endl;
-	cout << add<double>(i, j) << endl;
-
-	return 0;
+T add(T a, T b) {
+    return a + b;
 }
-
+int main() {
+    // 정수(int), 실수(double) 등 어떤 타입이든 이 함수 하나로 해결!
+    cout << add<int>(10, 20) << " ";     // int 버전
+    cout << add<double>(1.1, 2.2);          // double 버전
+    return 0;
+}
