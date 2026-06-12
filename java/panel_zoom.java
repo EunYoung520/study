7aed020c737acb2718b93090ea861226397adac3
EunYoung520 <165741784+EunYoung520@users.noreply.github.com>
2026-06-12 10:54:49 +0900

    Update and rename image_zoom.java to panel_zoom.java

--- java/panel_zoom.java
+++ java/panel_zoom.java
@@ -8,40 +8,51 @@ import java.awt.*;
 public class ImageViewer extends JFrame {
 	
 	private JLabel imagelabel;
+	private int panelwidth;
+	private int panelheight;
 	
 	public ImageViewer() {
-		setTitle("action");
+		setTitle("Wheelevwnt");
 		Container c=getContentPane();
-		c.setLayout(new BorderLayout());
-		
+		c.setLayout(null);
+	
 		setSize(1200,800);
-		
+		setLocationRelativeTo(null);
 		JPanel leftpanel=new JPanel();
-		leftpanel.setLayout(new BorderLayout());
 		imagelabel = new JLabel();
-		imagelabel.setHorizontalAlignment(JLabel.CENTER);
-		imagelabel.setVerticalAlignment(JLabel.CENTER);
 		ImageIcon icon =new ImageIcon("C:\\Users\\user\\Pictures\\1000035587.jpg"); 
 		imagelabel.setIcon(icon);
-		leftpanel.add(imagelabel, BorderLayout.CENTER);
-		c.add(leftpanel, BorderLayout.CENTER);
-		leftpanel.addMouseWheelListener(new MouseWheelListener() {
+		leftpanel.setBounds(100, 100, 300, 400);
+		
+		panelwidth=leftpanel.getWidth();
+		panelheight=leftpanel.getHeight();
+		
+		c.add(leftpanel);
+		leftpanel.add(imagelabel);
+		
+		c.addMouseWheelListener(new MouseWheelListener() {
 		    @Override
 		    public void mouseWheelMoved(MouseWheelEvent e) {
 		        int notches = e.getWheelRotation();
-		        int delta = 5;
+		        int delta = 100;
 
 		        if(notches < 0) {
-		            
+		        	panelwidth +=delta;
+		        	panelheight +=delta;
 		        }
+		            
 		        else {
-		            
+		        	panelwidth -=delta;
+		        	panelheight -=delta;		        	
 		        }
+		        leftpanel.setBounds(leftpanel.getX(), leftpanel.getY(),panelwidth, panelheight);
 		    }
 		});
+		
+		setVisible(true);
+	}
 	
 	public static void main(String[] args) {
 		ImageViewer sw = new ImageViewer(); 
 	}
 }
-
